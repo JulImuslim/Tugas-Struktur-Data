ebcb527055a14b3483ccd67add5aceda33c306d6
JulImuslim <127715565+JulImuslim@users.noreply.github.com>
2023-06-04 17:32:48 +0700

    Add files via upload

---
+++ luassegitiga.py
@@ -0,0 +1,9 @@
+a=float(input("masukan panjang alas:"))
+t=float(input("masukan tingi segitiga:"))
+luas=3.5*a*t
+print("luas segitiga adalah:"+str(luas))
+    
+alas=float(input('input alas segiempat:'))
+tinggi=float(input('input tinggi segiempat:'))
+luas=2.4
+print('luas segiempat=',round(luas,2))
